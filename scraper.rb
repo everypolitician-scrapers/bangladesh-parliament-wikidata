84bd6acbb34acba57d30a2d2434e9db0f1f263e3
Tony Bowden <tony@tmtm.com>
2016-05-20 19:47:31 +0100

    Initial scraper

---
+++ scraper.rb
@@ -0,0 +1,8 @@
+require 'wikidata/fetcher'
+
+en = WikiData::Category.new( 'Category:Members of the Jatiyo Sangshad', 'en').member_titles
+de = WikiData::Category.new( 'Kategorie:Abgeordneter (Bangladesch)', 'de').member_titles
+bn = WikiData::Category.new( 'বিষয়শ্রেণী:জাতীয় সংসদ সদস্য', 'bn').member_titles
+
+EveryPolitician::Wikidata.scrape_wikidata(names: { en: en, de: de, bn: bn }, output: false)
+
