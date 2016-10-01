6e1b8e64953396d8f56e22a4b02accbc07385d56
Tony Bowden <tony@tmtm.com>
2016-10-01 15:40:27 +0100

    Also scrape Speakers of the Jatiyo Sangshad

--- scraper.rb
+++ scraper.rb
@@ -1,6 +1,7 @@
 require 'wikidata/fetcher'
 
-en = WikiData::Category.new( 'Category:Members of the Jatiyo Sangshad', 'en').member_titles
+en = WikiData::Category.new( 'Category:Members of the Jatiyo Sangshad', 'en').member_titles |
+     WikiData::Category.new( 'Category:Speakers of the Jatiyo Sangshad', 'en').member_titles
 de = WikiData::Category.new( 'Kategorie:Abgeordneter (Bangladesch)', 'de').member_titles
 bn = WikiData::Category.new( 'বিষয়শ্রেণী:জাতীয় সংসদ সদস্য', 'bn').member_titles
 
