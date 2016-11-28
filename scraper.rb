0dbcf078f0a43a2f5ea00d038a6f9674ce1d0f94
Tony Bowden <tony@tmtm.com>
2016-11-28 11:48:21 +0000

    Also scrape 'Women members of the Jatiyo Sangshad'

--- scraper.rb
+++ scraper.rb
@@ -4,6 +4,7 @@ require 'wikidata/fetcher'
 existing = EveryPolitician::Index.new.country("Bangladesh").lower_house.popolo.persons.map(&:wikidata).compact
 
 en = WikiData::Category.new( 'Category:Members of the Jatiyo Sangshad', 'en').member_titles |
+     WikiData::Category.new( 'Category:Women members of the Jatiyo Sangshad', 'en').member_titles |
      WikiData::Category.new( 'Category:Speakers of the Jatiyo Sangshad', 'en').member_titles
 de = WikiData::Category.new( 'Kategorie:Abgeordneter (Bangladesch)', 'de').member_titles
 bn = WikiData::Category.new( 'বিষয়শ্রেণী:জাতীয় সংসদ সদস্য', 'bn').member_titles
