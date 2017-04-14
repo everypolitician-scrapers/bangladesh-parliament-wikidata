c9fb883b9185bdbde15641b0b395c0d8d4f24277
Tony Bowden <tony@tmtm.com>
2017-04-14 06:46:39 +0100

    Also find people via P39s

--- scraper.rb
+++ scraper.rb
@@ -9,5 +9,20 @@ en = WikiData::Category.new( 'Category:Members of the Jatiya Sangshad', 'en').me
 de = WikiData::Category.new( 'Kategorie:Abgeordneter (Bangladesch)', 'de').member_titles
 bn = WikiData::Category.new( 'বিষয়শ্রেণী:জাতীয় সংসদ সদস্য', 'bn').member_titles
 
-EveryPolitician::Wikidata.scrape_wikidata(ids: existing, names: { en: en, de: de, bn: bn })
+# Find all P39s of the 10th Parliament
+query = <<EOS
+  SELECT DISTINCT ?item
+  WHERE
+  {
+    BIND(wd:Q21272758 AS ?membership)
+    BIND(wd:Q29387197 AS ?term)
+
+    ?item p:P39 ?position_statement .
+    ?position_statement ps:P39 ?membership .
+    ?position_statement pq:P2937 ?term .
+  }
+EOS
+p39s = EveryPolitician::Wikidata.sparql(query)
+
+EveryPolitician::Wikidata.scrape_wikidata(ids: existing | p39s, names: { en: en, de: de, bn: bn })
 
