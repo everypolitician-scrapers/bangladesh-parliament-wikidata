fe501c87da169623a6c2dce4063ea22d622f8a49
Tony Bowden <tony@tmtm.com>
2018-08-19 13:25:18 +0100

    Update Membership Qid
    The item for being a Member has changed.

--- scraper.rb
+++ scraper.rb
@@ -9,7 +9,7 @@ en = WikiData::Category.new( 'Category:Members of the Jatiya Sangsad', 'en').mem
 de = WikiData::Category.new( 'Kategorie:Abgeordneter (Bangladesch)', 'de').member_titles
 bn = WikiData::Category.new( 'বিষয়শ্রেণী:জাতীয় সংসদ সদস্য', 'bn').member_titles
 
-query = 'SELECT DISTINCT ?item WHERE { ?item p:P39 [ ps:P39/wdt:P279 wd:Q21272758 ] .  }'
+query = 'SELECT DISTINCT ?item WHERE { ?item p:P39 [ ps:P39/wdt:P279 wd:Q13058882 ] .  }'
 p39s = EveryPolitician::Wikidata.sparql(query)
 
 EveryPolitician::Wikidata.scrape_wikidata(ids: existing | p39s, names: { en: en, de: de, bn: bn })
