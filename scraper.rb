51463e99d9ee90bf3f923fd78bc070625ce56a0b
Tony Bowden <tony@tmtm.com>
2019-02-05 15:23:58 +0000

    Switch to using IDs throughout

--- scraper.rb
+++ scraper.rb
@@ -3,12 +3,12 @@ require 'wikidata/fetcher'
 
 existing = EveryPolitician::Index.new.country("Bangladesh").lower_house.popolo.persons.map(&:wikidata).compact
 
-en = WikiData::Category.new( 'Category:9th Jatiya Sangsad members', 'en').member_titles |
-     WikiData::Category.new( 'Category:10th Jatiya Sangsad members', 'en').member_titles
-de = WikiData::Category.new( 'Kategorie:Abgeordneter (Bangladesch)', 'de').member_titles
-bn = WikiData::Category.new( 'বিষয়শ্রেণী:জাতীয় সংসদ সদস্য', 'bn').member_titles
+en = WikiData::Category.new( 'Category:9th Jatiya Sangsad members', 'en').wikidata_ids |
+     WikiData::Category.new( 'Category:10th Jatiya Sangsad members', 'en').wikidata_ids
+de = WikiData::Category.new( 'Kategorie:Abgeordneter (Bangladesch)', 'de').wikidata_ids
+bn = WikiData::Category.new( 'বিষয়শ্রেণী:জাতীয় সংসদ সদস্য', 'bn').wikidata_ids
 
-query = 'SELECT DISTINCT ?item WHERE { ?item p:P39 [ ps:P39/wdt:P279 wd:Q13058882 ] .  }'
+query = 'SELECT DISTINCT ?item WHERE { ?item p:P39 [ ps:P39/wdt:P279 wd:Q13058882 ] }'
 p39s = EveryPolitician::Wikidata.sparql(query)
 
-EveryPolitician::Wikidata.scrape_wikidata(ids: existing | p39s, names: { en: en, de: de, bn: bn })
+EveryPolitician::Wikidata.scrape_wikidata(ids: existing | p39s | en | de | bn)
