c65cb6ba333431434b44d91fa2d433f2dcc739c6
Tony Bowden <tony@tmtm.com>
2019-02-05 15:24:44 +0000

    Don't get from dewiki Categories
    The only people unique to this source are from terms earlier than we
    want.

--- scraper.rb
+++ scraper.rb
@@ -5,10 +5,9 @@ existing = EveryPolitician::Index.new.country("Bangladesh").lower_house.popolo.p
 
 en = WikiData::Category.new( 'Category:9th Jatiya Sangsad members', 'en').wikidata_ids |
      WikiData::Category.new( 'Category:10th Jatiya Sangsad members', 'en').wikidata_ids
-de = WikiData::Category.new( 'Kategorie:Abgeordneter (Bangladesch)', 'de').wikidata_ids
 bn = WikiData::Category.new( 'বিষয়শ্রেণী:জাতীয় সংসদ সদস্য', 'bn').wikidata_ids
 
 query = 'SELECT DISTINCT ?item WHERE { ?item p:P39 [ ps:P39/wdt:P279 wd:Q13058882 ] }'
 p39s = EveryPolitician::Wikidata.sparql(query)
 
-EveryPolitician::Wikidata.scrape_wikidata(ids: existing | p39s | en | de | bn)
+EveryPolitician::Wikidata.scrape_wikidata(ids: existing | p39s | en | bn)
